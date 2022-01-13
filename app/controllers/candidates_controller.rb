365ea2d82a907ee4e59928fad8ee398982f1eb90
heber <heberlazo@gmail.com>
2022-01-12 20:15:52 -0500

    Added index to candidates controller

--- app/controllers/candidates_controller.rb
+++ app/controllers/candidates_controller.rb
@@ -1,4 +1,17 @@
 class CandidatesController < ApplicationController
   def index
+    years = CandidateSearchServices.calculate_days(candidate_params[:email])
+
+    render json: {
+      email: candidate_params[:email],
+      work_experience_years: years
+    }
+
+  end
+
+  private
+
+  def candidate_params
+    params.permit(:email)
   end
 end
